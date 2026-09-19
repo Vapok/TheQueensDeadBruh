b29125522f54a6b1f8ebd7511f62b3ec1134c3b2
Vapok <vapokrocks@gmail.com>
2026-09-19 00:20:01 -0400

    Release v2.0.7: Valheim 1.0.15 Alignment & Internalized Dependency Updates

--- TheQueensDeadBruh/TheQueensDeadBruh.cs
+++ TheQueensDeadBruh/TheQueensDeadBruh.cs
@@ -25,7 +25,7 @@ namespace TheQueensDeadBruh
         //Module Constants
         private const string _pluginId = "vapok.mods.thequeensdeadbruh";
         private const string _displayName = "The Queens Dead Bruh!";
-        private const string _version = "2.0.6";
+        private const string _version = "2.0.7";
         
         //Interface Properties
         public string PluginId => _pluginId;
